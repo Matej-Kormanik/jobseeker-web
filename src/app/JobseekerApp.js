24c5039bcc143e5e0aa070f793b2bc6faa2b56ff
Matej Kormanik <matej.kormanik@gmail.com>
2020-08-14 23:45:22 +0200

    First commit

---
+++ src/app/JobseekerApp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './JobseekerApp.css';
+
+function JobseekerApp() {
+  return (
+    <div className="App">
+      ahoj
+    </div>
+  );
+}
+
+export default JobseekerApp;
